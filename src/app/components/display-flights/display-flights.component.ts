e61b4747e37b2da75e531f2da9ed085a93be8ec6
Kyriakos Chatzisavvas <kchatzisavvas@novibet.com>
2023-03-29 17:31:20 +0300

    display flights

--- src/app/components/display-flights/display-flights.component.ts
+++ src/app/components/display-flights/display-flights.component.ts
@@ -1,15 +1,18 @@
+import { DatePipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-display-flights',
   templateUrl: './display-flights.component.html',
-  styleUrls: ['./display-flights.component.css']
+  styleUrls: ['./display-flights.component.css'],
+  providers: [DatePipe]
 })
 export class DisplayFlightsComponent {
-  
-  flightsData:any;
-  constructor(private router: Router) {
+  flightsData: any;
+  selectedFlightId: number = 0;
+
+  constructor(private router: Router, private datePipe: DatePipe) {
     const navigation = this.router.getCurrentNavigation();
     const state = navigation?.extras.state as {
       flightsData: string
@@ -18,7 +21,19 @@ export class DisplayFlightsComponent {
     console.log(this.flightsData);
   }
 
-  onSelect(id:number):any{
-    this.router.navigate(['/passengerDetails',id]);
+  onSelect(id: number): any {
+    this.router.navigate(['/passengerDetails', id]);
+  }
+
+  goBack(): void {
+    this.router.navigate(['/findFlights']);
+  }
+
+  formatDate(dateString: string): string {
+    return this.datePipe.transform(dateString, 'MMMM d, y, h:mm a') ?? "N/A";
+  }
+
+  onFlightClick(id: number): void {
+    this.selectedFlightId = id;
   }
 }
