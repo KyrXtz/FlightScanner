075417eb2ce350e32a479cdb559de785ea45c50d
Kyriakos Chatzisavvas <kchatzisavvas@novibet.com>
2023-04-10 14:31:44 +0300

    added airport name

--- src/app/components/display-flights/display-flights.component.ts
+++ src/app/components/display-flights/display-flights.component.ts
@@ -1,6 +1,8 @@
 import { DatePipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { map } from 'rxjs';
+import { LoadAirportsService } from 'src/app/services/load-airports.service';
 
 @Component({
   selector: 'app-display-flights',
@@ -12,14 +14,39 @@ export class DisplayFlightsComponent {
   flightsData: any;
   selectedFlightId: number = 0;
 
-  constructor(private router: Router, private datePipe: DatePipe) {
-    const navigation = this.router.getCurrentNavigation();
-    const state = navigation?.extras.state as {
-      flightsData: string
-    };
-    this.flightsData = JSON.parse(state.flightsData);
-    console.log(this.flightsData);
+  constructor(private router: Router, private datePipe: DatePipe, private airportsService: LoadAirportsService) {
+    this.airportsService.getAirports()
+      .pipe(
+        map((response: any) => 
+          response.map((airport: { code: string; name: string; country: string }) => {
+            return {
+              code: airport.code,
+              name: airport.name,
+              country: airport.country
+            };
+          })
+        )
+      )
+      .subscribe((airportsSelected: Array<{ code: string; name: string; country: string }>) => {
+        const navigation = this.router.getCurrentNavigation();
+        const state = navigation?.extras.state as {
+          flightsData: string
+        };
+        
+        const rawFlightsData = JSON.parse(state.flightsData);
+        this.flightsData = rawFlightsData.map((flight: any) => {
+          const departureAirport = airportsSelected.find(airport => airport.code === flight.departureCity);
+          const arrivalAirport = airportsSelected.find(airport => airport.code === flight.arrivalCity);
+          
+          return {
+            ...flight,
+            departureCity: departureAirport ? `${flight.departureCity} - ${departureAirport.name}` : flight.departureCity,
+            arrivalCity: arrivalAirport ? `${flight.arrivalCity} - ${arrivalAirport.name}` : flight.arrivalCity
+          };
+        });
+      });
   }
+  
 
   onSelect(id: number): any {
     this.router.navigate(['/passengerDetails', id]);
