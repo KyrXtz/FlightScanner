8aadbc3c3bfa826d4fec32db409ffe375f11bb97
Kyriakos Chatzisavvas <kchatzisavvas@novibet.com>
2023-05-12 13:48:12 +0300

    check in styling

--- src/app/components/check-in/check-in.component.ts
+++ src/app/components/check-in/check-in.component.ts
@@ -2,7 +2,9 @@ import { DatePipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { map } from 'rxjs';
 import { CheckInService } from 'src/app/services/check-in.service';
+import { LoadAirportsService } from 'src/app/services/load-airports.service';
 
 @Component({
   selector: 'app-check-in',
@@ -14,17 +16,38 @@ export class CheckInComponent {
 
   reservationData:any;
   checkInForm:FormGroup;
+  airports: { code: string, name: string, country: string}[] = [];
 
-  constructor(private formBuilder:FormBuilder, private router: Router, private checkInService:CheckInService, private datePipe: DatePipe) {
-    const navigation = this.router.getCurrentNavigation();
-    const state = navigation?.extras.state as {
-      reservationData: string
-    };
-    this.reservationData = JSON.parse(state.reservationData);
+  constructor(private formBuilder:FormBuilder,
+     private router: Router,
+     private checkInService:CheckInService,
+     private airportsService:LoadAirportsService,
+     private datePipe: DatePipe) {
+      this.airportsService.getAirports()
+        .pipe(
+          map((response: any) => 
+            response.map((airport: { code: string; name: string; country: string }) => {
+              return {
+                code: airport.code,
+                name: airport.name,
+                country: airport.country
+              };
+            })
+          )
+        )
+        .subscribe((airports: Array<{ code: string; name: string; country: string }>) => {
+          this.airports = airports;
+        });
 
-    this.checkInForm = this.formBuilder.group({
-      'numberOfBags' : ['',Validators.required]
-    })
+        const navigation = this.router.getCurrentNavigation();
+        const state = navigation?.extras.state as {
+          reservationData: string
+        };
+        this.reservationData = JSON.parse(state.reservationData);
+
+        this.checkInForm = this.formBuilder.group({
+          'numberOfBags' : ['',[Validators.required,Validators.minLength(1), Validators.maxLength(2)]]
+        })
 
   }
 
@@ -40,4 +63,8 @@ export class CheckInComponent {
     return this.datePipe.transform(dateString, 'MMMM d, y, h:mm a') ?? "N/A";
   }
 
+  getCity(airportCode: string){
+    const airport = this.airports.filter(ap => ap.code === airportCode)[0];
+    return airport ? `${airportCode} - ${airport.name}` : airportCode;
+  }
 }
